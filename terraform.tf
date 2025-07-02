4b892561a640cb24e615479a53308edab6bb2e2d
Gene Laisne <glaisne@opentext.com>
2025-07-02 17:09:00 -0400

    random provider, azurerm features for keyvault

--- terraform.tf
+++ terraform.tf
@@ -6,12 +6,21 @@ terraform {
             source = "hashicorp/azurerm"
             version = "~> 4.30.0"
         }
+        random = {
+            source = "hashicorp/random"
+            version = "~> 3.7.0"
+        }
     }
     backend "azurerm" {}
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_deleted_secrets_on_destroy = true
+      recover_soft_deleted_secrets          = true
+    }
+  }
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
