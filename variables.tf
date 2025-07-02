511caa38e6b05b9d83a1ed82910a2dee3f553458
Gene Laisne <glaisne@opentext.com>
2025-07-02 17:09:11 -0400

    sql variables

--- variables.tf
+++ variables.tf
@@ -11,11 +11,34 @@ variable "subscription_id" {
 variable "resource_group_name" {
     type = string
     description = "Name of the Resource Group Name"
-    default = "rg-terraform_project"
+    default = "rg-terraform_project03"
 }
 
 variable "location" {
     type = string
     description = "Location (or region) for the Azure resources"
     default = "eastus2"
+}
+
+
+#
+#   SQL Variables
+#
+
+
+variable "mssql_credentials" {
+    type = map
+    description = "Credentials for mssql server"
+}
+
+variable "mssql_sku" {
+    type = string
+    description = "SKU used for MSSQL managed service"
+    default = "Basic"
+}
+
+variable "mssql_collation" {
+    type = string
+    description = "Collation configuration used for MSSQL managed service"
+    default = "SQL_Latin1_General_CP1_CI_AS"
 }
